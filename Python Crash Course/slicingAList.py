f721104797fe277c9c686cee13e1741d74e7d31f
ngdangtuan <tuanhb20@gmail.com>
2017-06-06 17:12:25 +0700

    slicing a list

---
+++ Python Crash Course/slicingAList.py
@@ -0,0 +1,10 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3]) #['charles', 'martina', 'michael']
+#Without a starting index, Python starts at the beginning of the list:
+print(players[:4]) #['charles', 'martina', 'michael', 'florence']
+
+print(players[2:] #['michael', 'florence', 'eli']
+
+print(players[-3:] #['michael', 'florence', 'eli']
+
+#[start:end:step]
