d1ac7f2c051a9059ccc72f741ed57bf781467596
ngdangtuan <tuanhb20@gmail.com>
2017-06-06 17:33:06 +0700

    slicing and copy a list

--- Python Crash Course/slicingAndCopyAList.py
+++ Python Crash Course/slicingAndCopyAList.py
@@ -1,3 +1,4 @@
+#SLICING
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3]) #['charles', 'martina', 'michael']
 #Without a starting index, Python starts at the beginning of the list:
@@ -8,3 +9,10 @@ print(players[2:] #['michael', 'florence', 'eli']
 print(players[-3:] #['michael', 'florence', 'eli']
 
 #[start:end:step]
+
+#COPY
+myFoods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+#NOTE: THIS DOESN'T WORK friend_foods = my_foods
+
+			
