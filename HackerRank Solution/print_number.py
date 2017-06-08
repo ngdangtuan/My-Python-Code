ec3e0b568a2c70fd42db0ad6dc40b019c557e9b7
Dang Tuan <tuanhb20@gmail.com>
2017-06-08 21:42:59 +0700

    Create print_number.py

--- HackerRank Solution/print_number.py
+++ HackerRank Solution/print_number.py
@@ -2,3 +2,9 @@ def print_numbers():
 	print(*range(1, int(input())+1), sep='')
 print_numbers()
 
+#Another solution
+n = int(input())
+def print_numbers(n):
+    numbers = list(range(1, n+1))
+    return ''.join(str(i) for i in numbers)
+print(print_numbers(n)
