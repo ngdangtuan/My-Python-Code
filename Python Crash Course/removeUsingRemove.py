6ab04779e8fcaa75854cde186b5e700994010670
Dang Tuan <tuanhb20@gmail.com>
2017-06-08 15:47:18 +0700

    Delete removeUsingRemove.py

--- Python Crash Course/removeUsingRemove.py
+++
@@ -1,5 +0,0 @@
-motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
-tooExpensive = 'ducati' #store the value 'ducati' to reuse
-motorcycles.remove(tooExpensive)
-print(motorcycles) #['honda', 'yamaha', 'suzuki']
-print("A" + tooExpensive.title() + " is too expensive for me.") #A Ducati is too expensive for me.
