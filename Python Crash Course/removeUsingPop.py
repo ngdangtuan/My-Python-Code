1a1d768ea617df716673b7961da9d98747fd8c46
Dang Tuan <tuanhb20@gmail.com>
2017-06-08 15:46:15 +0700

    Delete removeUsingPop.py

--- Python Crash Course/removeUsingPop.py
+++
@@ -1,7 +0,0 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
-poppedMotorcycle = motorcycles.pop()
-print(motorcycles) #['honda', 'yamaha']
-print(poppedMotorcycle) #suzuki
-#Popping Items from any Position in List
-firstOwned = motorcycles.pop(0)
-print(firstOwned) #honda
